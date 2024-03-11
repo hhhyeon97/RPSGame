c4f96b8f26d10cd745a16d504e2224f7fe16df68
jihyeon <kth083030@naver.com>
2024-03-11 22:23:07 +0900

    item선택

--- src/App.js
+++ src/App.js
@@ -10,8 +10,31 @@ import Box from './component/Box'; // 박스 컴포넌트 불러오기
 // 6. 승패 결과에 따라 테두리 색이 바뀐다. (이기면 초록, 지면 빨강, 비기면 검정)
 
 // jsx 컴포넌트는 반드시 하나의 덩어리를 리턴해야 한다.
+// onclick x - >  ☆ onClick
+
+// 함수를 호출하는 형식으로 넣으면 UI를 랜더링할 때 바로 함수가 실행되어 버림
+// 함수를 전달하는 형식 사용 - > 콜백함수 형태로 함수를 전달해줘야 한다 !
+// EX) onClick={() => play('scissors')}
+
+const choice = {
+  rock: {
+    name: 'Rock',
+    img: 'https://i.pinimg.com/564x/59/7d/6d/597d6d18da21653580bc120569c08d45.jpg',
+  },
+  paper: {
+    name: 'Paper',
+    img: 'https://i.pinimg.com/564x/d1/b3/ff/d1b3fff5d18e54411732680f12f0257a.jpg',
+  },
+  scissors: {
+    name: 'Scissors',
+    img: 'https://i.pinimg.com/564x/7e/c6/95/7ec6954e96ca40a53523a8028cad5dca.jpg',
+  },
+};
 
 function App() {
+  const play = (userChoice) => {
+    console.log('선택됨!', userChoice);
+  };
   return (
     <div>
       <div className="main">
@@ -19,9 +42,9 @@ function App() {
         <Box title="computer" />
       </div>
       <div className="main">
-        <button>가위</button>
-        <button>바위</button>
-        <button>보</button>
+        <button onClick={() => play('scissors')}>가위</button>
+        <button onClick={() => play('rock')}>바위</button>
+        <button onClick={() => play('paper')}>보</button>
       </div>
     </div>
   );
